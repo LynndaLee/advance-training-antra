944bf66ca40d7e8b5700c33af4a0dc0ce306dd68
LynndaLee <91641701+LynndaLee@users.noreply.github.com>
2023-04-20 13:42:02 -0400

    npm run build wasn't working

---
+++ day-two/babel/lib/hw2-index.js
@@ -0,0 +1,9 @@
+// console.log(React)
+// console.log(ReactDOM)
+class HelloMessage extends React.Component {
+  render() {
+    return React.createElement("div", null, "Hello World");
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(React.createElement(HelloMessage, null));
