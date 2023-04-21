8a4c293f1ca8475f810f9b9e2509ad843308804b
LynndaLee <91641701+LynndaLee@users.noreply.github.com>
2023-04-21 14:41:33 -0400

    update

--- day3webpack/src/index.js
+++ day3webpack/src/index.js
@@ -17,30 +17,28 @@
 
 // }}
 
-// import Counter from "./components/Counter.js"; 
+import Counter from "./components/Counter.js"; 
 
 
-// class HelloMessage extends React.Component {
-//     render() {
-//       return (
-//         <div>
-//           <h1>Hello World</h1>
-//           <Counter /> 
-//         </div>
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello World</h1>
+          <Counter /> 
+        </div>
 
-//       );
-//     }
-//   }
-
-
+      );
+    }
+  }
 
 
-// ReactDOM.render(<HelloMessage />, document.getElementById("app"));
+
+
+ReactDOM.render(<HelloMessage />, document.getElementById("app"));
 
 //new updated version did not work 
 //   const root = ReactDOM.createRoot(document.getElementById("app"));
 //   root.render(<HelloMessage />);
 
 
-import { sum } from "./util/math";
-console.log(sum(1,2));
